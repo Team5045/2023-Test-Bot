50286838a7c552fb276b03a4b74271e9580fe484
Pronkle <allenleiguo@gmail.com>
2023-03-24 21:47:34 -0500

    added several gyro changes for testing

--- components/gyro.py
+++ components/gyro.py
@@ -14,18 +14,34 @@ class gyro():
     navx = navx.AHRS.create_spi()
     
     def balancing(self):
+        self.navx.reset()
         angle = self.navx.getPitch()
-        if angle > 0:
-            self.drivetrain.set_motors(0.2, 0.0)
-            self.sd.putValue("Mode: ", "Moving Forward")
-        elif angle < 0:
-            self.drivetrain.set_motors(-0.2, 0.0)
-            self.sd.putValue("Mode: ", "Moving Backward")
+        ABSOLUTE_ANGLE = abs(angle)
+        MAX_RANGE_LIM = 180
+        ''' Just making things a bit easier to read here with variables'''
+        print(angle)
+
+        motor_sets = angle/MAX_RANGE_LIM
+        # This should provide constant adjustment to the speeds so it won't jerk the robot
+
+        if (ABSOLUTE_ANGLE > 0) and (ABSOLUTE_ANGLE < MAX_RANGE_LIM):
+            if angle > 0.0:
+                self.drivetrain.set_motors(motor_sets, 0.0)
+                self.sd.putValue("Mode: ", "Moving Forward")
+
+            elif angle < 0.0:
+                self.drivetrain.set_motors(motor_sets, 0.0)
+                self.sd.putValue("Mode: ", "Moving Backward")
+
+            else:
+                self.drivetrain.set_motors(0.0, 0.0)
+                self.sd.putValue("Mode: ", "Balanced!")
+                print("balanced!")
+
+            ''' Conditional Statements after prereqs are checked '''
         else:
             self.drivetrain.set_motors(0.0, 0.0)
-            self.sd.putValue("Mode: ", "Balanced!")
-            print("balanced!")
-        print(self.navx.getPitch())
+            
 
         
         
